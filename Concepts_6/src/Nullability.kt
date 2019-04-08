35715442e89a1b791e1d329507ef2d9221b0c849
David Barzi <barzi.david@gmail.com>
2019-04-09 00:00:06 +0300

    Completed exercise "Practice Time: Nullability" from section 6 of chapter "Concepts"

---
+++ Concepts_6/src/Nullability.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    var rainbowColor = "Purple"
+
+    // rainbowColor is not nullable
+//    rainbowColor = null
+
+    var greenColor = null
+
+    var blueColor : String? = null
+}
