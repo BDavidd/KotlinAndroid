c8994562b79b41295e47aa5704d9f4a6e3f8d09c
David Barzi <barzi.david@gmail.com>
2019-04-08 23:38:21 +0300

    Added the correct syntax.

--- Concepts_6/src/BasicOperations.kt
+++ Concepts_6/src/BasicOperations.kt
@@ -1,11 +1,11 @@
 fun main()
 {
-    val noOfFish = 2 + 71 + 233 - 13
+    val noOfFish = 2.plus(71).plus(233).minus(13)
     println("number of fish: $noOfFish")
 
-    val aquariumsDifferentLine = noOfFish / 30
+    val aquariumsDifferentLine = noOfFish.div(30).plus(1)
     println("number of aquariums separately computed: $aquariumsDifferentLine")
 
-    val aquariumsSameLine = (2 + 71 + 233 - 13) / 30
+    val aquariumsSameLine = 2.plus(71).plus(233).minus(13).div(30).plus(1)
     println("number of aquariums computed in the same line: $aquariumsSameLine")
 }
