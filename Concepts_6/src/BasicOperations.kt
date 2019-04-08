6199bdf51dcb73966dcc661e03e2937a3894fc34
David Barzi <barzi.david@gmail.com>
2019-04-08 23:17:38 +0300

    Completed exercise "Practice Time: Basic Operations" from section 6 of chapter "Concepts"

---
+++ Concepts_6/src/BasicOperations.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    val noOfFish = 2 + 71 + 233 - 13
+    println("number of fish: $noOfFish")
+
+    val aquariumsDifferentLine = noOfFish / 30
+    println("number of aquariums separately computed: $aquariumsDifferentLine")
+
+    val aquariumsSameLine = (2 + 71 + 233 - 13) / 30
+    println("number of aquariums computed in the same line: $aquariumsSameLine")
+}
