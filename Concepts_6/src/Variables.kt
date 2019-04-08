297d370edddef1aa786a0ed04960abf40693d63c
David Barzi <barzi.david@gmail.com>
2019-04-08 23:44:00 +0300

    Completed exercise "Practice Time: Variables" from section 6 of chapter "Concepts"

---
+++ Concepts_6/src/Variables.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    var rainbowColor = "Purple"
+    println("Before updating: $rainbowColor")
+
+    rainbowColor = "Blue"
+    println("After updating: $rainbowColor")
+
+    val blackColor = "Black"
+    println("The darkest color: $blackColor")
+
+//    blackColor = "White"
+//    println("After updating: $blackColor")
+}
